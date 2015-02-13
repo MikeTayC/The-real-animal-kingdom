b0ef67a796c29b0599b1990f9f7c9b5091c4c74a
MikeTayC <miketayc@gmail.com>
2015-02-12 20:04:50 -0500

    updating with functions setPetName and getPetName

--- Animal.class.php
+++ Animal.class.php
@@ -3,7 +3,21 @@
   class Animal
   {
     public $isAlive = true;
+    
     public $petName;
+    
+    public function setPetName($newPetName)
+    {
+      $this->petName = $newPetName; 
+    }
+    
+    public function getPetName()
+    {
+      return $this->petName;
+    }
+    
   }
+    
+  
   
 ?>
